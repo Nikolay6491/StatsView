b0690efafd2c95a13d7387bc1403b0af5ba7b144
Nikolay6491 <nikshustov@gmail.com>
2023-08-18 14:56:21 +0400

    Custom Views

---
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.netology.statsview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.statsview.ui.StatsView
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val statsView = findViewById<StatsView>(R.id.statsView)
+
+        statsView.data = listOf(500f, 500f, 500f, 500f)
+    }
+}
