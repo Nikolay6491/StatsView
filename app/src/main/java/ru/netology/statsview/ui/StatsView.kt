0c94db716ff2c000dd63cf40e1693731698ce361
Nikolay6491 <nikshustov@gmail.com>
2023-08-23 18:22:38 +0400

    Animations #2

--- app/src/main/java/ru/netology/statsview/ui/StatsView.kt
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -87,7 +87,7 @@ class StatsView @JvmOverloads constructor(
         var startAngle = -90F
         data.forEachIndexed { index, d ->
             paint.color = colors.getOrElse(index) { generateRandomColor() }
-            canvas.drawArc(oval, startAngle + progress * 360F, d * 360F, false, paint)
+            canvas.drawArc(oval, startAngle , d * 360F * progress, false, paint)
             startAngle += d * 360F
         }
 
@@ -99,7 +99,7 @@ class StatsView @JvmOverloads constructor(
         }
 
         canvas.drawText(
-            "%.2f%%".format(data.sum() * 100),
+            "%.2f%%".format(data.sum() * 100 * progress),
             center.x,
             center.y + textPaint.textSize / 4,
             textPaint
